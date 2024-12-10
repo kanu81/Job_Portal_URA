0295ff590412e5aff098aec0686d84c1f7aa648b
Tanishjot <tanishjotbrar@gmail.com>
2024-12-10 20:49:07 +0530

    Added NodeMailer and fixed bugs

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,5 +1,4 @@
-// import express from "express";
-// import { login, logout, register, updateProfile } from "../controllers/user.controller.js";
+
 import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 import express from "express";
